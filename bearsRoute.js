8c7040890dcafa1b1c5775e32a89cbcd54d59b2f
cecil john tantay <cjbtantay@gmail.com>
2019-02-19 11:22:37 -0800

    add bears get by id route

--- bearsRoute.js
+++ bearsRoute.js
@@ -12,7 +12,22 @@ route.get('/', (req, res) => {
     });
 });
 
-route.get('/:id', (req, res) => {});
+route.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db('bears')
+    .where({ id })
+    .first()
+    .then(ids => {
+      if (ids) {
+        res.status(200).json(ids);
+      } else {
+        res.status(404).json({ message: 'id not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 route.post('/', (req, res) => {});
 
