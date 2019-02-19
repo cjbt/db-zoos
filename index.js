f71e7dfeaee28ecd2a48d70ccf6046651125d2b7
cecil john tantay <cjbtantay@gmail.com>
2019-02-19 10:33:11 -0800

    add get route

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/', (req, res) => {
+  res.send("it's working");
+});
+
+// getAll
+server.get('/zoos', (req, res) => {
+  db('zoos')
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// getById
+
+// post
+
+// update
+
+// delete
 
 const port = 3300;
 server.listen(port, function() {
